2c05139bde4a7006e0d65a84eb0a2d303fa08b8d
Bosun Akinyemi <azurnwa.p@gmail.com>
2020-08-19 14:05:43 +0100

    Atempted Bug Fix

--- server/__init__.py
+++ server/__init__.py
@@ -10,7 +10,7 @@ def create_app():
     @app.after_request
     def after_request(response):
         response.headers.add('Access-Control-Allow-Origin',
-                             'http://localhost:3000')
+                             'https://filter-frontend.vercel.app')
         response.headers.add('Access-Control-Allow-Headers',
                              'Content-Type,Authorization')
         response.headers.add('Access-Control-Allow-Methods',
